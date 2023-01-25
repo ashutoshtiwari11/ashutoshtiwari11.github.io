aff49122330c7aef44e335f02941e28df699ee86
ashutoshtiwari11 <115335561+ashutoshtiwari11@users.noreply.github.com>
2023-01-25 15:16:53 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -2,7 +2,7 @@
     key: "fcc8de7015bbb202209bbf0261babf4c",
     base: "https://api.openweathermap.org/data/2.5/"
   }
-}
+
 
 const searchbox = document.querySelector('.search-box');
 searchbox.addEventListener('keypress', setQuery);
